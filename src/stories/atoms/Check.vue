4abd40679645d142c109562858e5f8dc5ff56d9a
camslice <cam@madebycam.com>
2021-05-07 23:13:08 +0700

    remove cruft

--- src/stories/atoms/Check.vue
+++ src/stories/atoms/Check.vue
@@ -4,10 +4,3 @@
     <path d="M9.1 12c-.3-.3-.7-.3-1 0-.3.3-.2.8 0 1l2.7 2.7c.3.3.8.3 1-.1l4.7-6.5c.2-.3.2-.8-.1-1-.3-.2-.7-.2-.9.1l-4.3 5.9L9.1 12z" fill="#fff"/>
   </svg>
 </template>
-<!--
-
-<script>
-export default {
-  name: 'icon-check'
-}
-</script>-->
--- src/stories/atoms/Close.vue
+++ src/stories/atoms/Close.vue
@@ -4,10 +4,3 @@
     <path d="M15.1 16.3L12 13.2l-3.1 3.1c-.3.3-.9.3-1.2 0-.3-.3-.3-.9 0-1.2l3.1-3.1-3.1-3.1c-.3-.3-.3-.9 0-1.2.3-.3.9-.3 1.2 0l3.1 3.1 3.1-3.1c.3-.3.9-.3 1.2 0s.3.9 0 1.2L13.2 12l3.1 3.1c.3.3.3.9 0 1.2s-.9.3-1.2 0z" fill-rule="evenodd" clip-rule="evenodd" fill="#fff"/>
   </svg>
 </template>
-<!--
-
-<script>
-export default {
-  name: 'icon-close',
-};
-</script>-->
