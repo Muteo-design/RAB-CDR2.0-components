8bce9d017e4995d774c51075f2f0d29789785e67
camslice <cam@madebycam.com>
2021-05-17 20:14:42 +0700

    ComponentTickbox v0.1.1 - fix closing tags

--- src/stories/atoms/Tickbox.js
+++ src/stories/atoms/Tickbox.js
@@ -1,50 +1,53 @@
-// ComponentTickbox
-// [[
-export default/*vueJSWidget.registerComponent('tickbox',*/ {
-  // v0.1.0
-  template: `
-    <div class="rounded p-2"
-         @mouseenter="hover = true"
-         @mouseleave="hover = false"
-         :class="[ disabled ? 'cursor-not-allowed' : 'cursor-pointer hover-bg-brand-secondary-1' ]">
-      <i class="icon-24" :class="iconName"/>
-      <input type="checkbox" class="d-none" v-model="checkedLocal" :disabled="disabled"/>
-    </div>
-  `,
-  props: {
-    checked: Boolean,
-    disabled: Boolean,
-  },
-  data: function() {
-    return {
-      hover: false,
-      isChecked: this.checked,
-    };
-  },
-  methods: {
-    setHover: function(value) {
-      if (!this.disabled) {
-        this.hover = value;
-      }
-    },
-  },
-  computed: {
-    iconName: function() {
-      if (this.isChecked) {
-        return 'icon-rab-tickbox-active' + (this.disabled ? '-disabled' : '');
-      } else {
-        return 'icon-rab-tickbox' + (this.disabled ? '-disabled' : (this.hover ? '-hover' : ''));
-      }
-    },
-    checkedLocal: {
-      get: function() {
-        this.isChecked = this.checked;
-        return this.checked;
-      },
-      set: function(value) {
-        this.isChecked = value;
-        this.$emit('update:checked', value);
-      },
-    },
-  },
+/*
+ComponentTickbox
+	[[
+		vueJSWidget.registerComponent('tickbox', {
+*/
+export default {
+	// v0.1.1
+	template: `
+		<div class="rounded p-2"
+			@mouseenter="hover = true"
+			@mouseleave="hover = false"
+			:class="[ disabled ? 'cursor-not-allowed' : 'cursor-pointer hover-bg-brand-secondary-1' ]">
+			<i class="icon-24" :class="iconName"></i>
+			<input type="checkbox" class="d-none" v-model="checkedLocal" :disabled="disabled"/>
+		</div>
+	`,
+	props: {
+		checked: Boolean,
+		disabled: Boolean,
+	},
+	data: function() {
+		return {
+			hover: false,
+			isChecked: this.checked,
+		};
+	},
+	methods: {
+		setHover: function(value) {
+			if (!this.disabled) {
+				this.hover = value;
+			}
+		},
+	},
+	computed: {
+		iconName: function() {
+			if (this.isChecked) {
+				return 'icon-rab-tickbox-active' + (this.disabled ? '-disabled' : '');
+			} else {
+				return 'icon-rab-tickbox' + (this.disabled ? '-disabled' : (this.hover ? '-hover' : ''));
+			}
+		},
+		checkedLocal: {
+			get: function() {
+				this.isChecked = this.checked;
+				return this.checked;
+			},
+			set: function(value) {
+				this.isChecked = value;
+				this.$emit('update:checked', value);
+			},
+		},
+	},
 };
