c30ff5a662daacdc4ab7f5d2f668bf92e3481d11
camslice <cam@madebycam.com>
2021-05-21 09:06:33 +0700

    dataholder-details v0.3.0

--- src/stories/molecules/DataholderDetails.js
+++ src/stories/molecules/DataholderDetails.js
@@ -4,12 +4,10 @@ ComponentVue_DataholderDetails
 		vueJSWidget.registerComponent('dataholder-details', {
 */
 export default {
-	// v0.2.2
+	// v0.3.0
 	template: `
 		<div class="dataholder-details d-flex align-items-center">
-			<div class="dataholder-logo flex-none rounded-circle overflow-hidden position-relative mr-2"
-				:class="[ smallLogo ? 'icon-24' : 'icon-32' ]">
-				<canvas ref="canvas"></canvas>
+			<div class="flex-none rounded-circle position-relative mr-2" :class="[ smallLogo ? 'icon-24' : 'icon-32' ]">
 				<div class="w-100 h-100 rounded-circle overflow-hidden position-relative bg-white border-white">
 					<img :src="dataholder.imageUrl" @load="setBrand($event)" class="center-xy"/>
 				</div>
@@ -22,14 +20,43 @@ export default {
 		smallLogo: Boolean,
 		nameClass: String,
 	},
+	data: function() {
+		return {
+			getAvgRgb: null,
+		};
+	},
+	mounted: function() {
+		this.getAvgRgb = function(img) {
+			var ctx = document.createElement('canvas').getContext('2d');
+			if (typeof img == 'string') {
+				var src = img;
+				img = new Image;
+				img.src = src;
+			}
+			ctx.imageSmoothingEnabled = true;
+			ctx.drawImage(img, 0, 0, 1, 1);
+			return ctx.getImageData(0, 0, 1, 1).data.slice(0, 3);
+		};
+	},
 	methods: {
 		setBrand: function(event) {
-			var canvas = this.$refs['canvas'];
-			canvas.setAttribute('width', '1px');
-			canvas.setAttribute('height', '1px');
-			var ctx = canvas.getContext('2d');
 			var image = event.currentTarget;
-			ctx.drawImage(image, 0, 0, 1, 1);
+			var rgb = this.getAvgRgb(image);
+			// If colour is too light or too dark, use fallback colour: brand-tertiary-3
+			if (((rgb[0] + rgb[1] + rgb[2]) > 715) || (rgb[0] + rgb[1] + rgb[2]) < 50) {
+				rgb[0] = 2;
+				rgb[1] = 145;
+				rgb[2] = 173;
+			}
+			var rgbString = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+			var styleBorder = 'border: 1px solid ' + rgbString + ';'
+			// Add box-shadow to emulate 0.5px
+			var styleShadow = 'box-shadow: 0 0 1px ' + rgbString +
+				',0.25px 0.25px 0 ' + rgbString +
+				',-0.25px 0.25px 0 ' + rgbString +
+				',-0.25px -0.25px 0 ' + rgbString +
+				',0.25px -0.25px 0 ' + rgbString + ';'
+			image.parentNode.parentNode.setAttribute('style', styleBorder + styleShadow);
 		},
 	},
 };
