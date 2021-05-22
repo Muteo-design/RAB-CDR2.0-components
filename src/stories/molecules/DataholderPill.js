0b7bd95c8313e5e9026d89ce95e8152243a2daca
camslice <cam@madebycam.com>
2021-05-23 01:10:28 +0700

    DataholderPill v0.1.1

--- src/stories/molecules/DataholderPill.js
+++ src/stories/molecules/DataholderPill.js
@@ -4,12 +4,12 @@ ComponentVue_DataholderPill
 		vueJSWidget.registerComponent('dataholder-pill', {
 */
 export default {
-	// v0.1.0
+	// v0.1.1
 	template: `
 		<div class="dataholder-pill rounded-pill border-brand-primary-1 bg-white p-2 mr-2 mb-2 mw-100"
 			:class="{ 'hover-shadow-2': editing && !disabled }">
 			<div class="d-flex align-items-center justify-content-between">
-				<dataholder-details :dataholder="dataholder" smallLogo :name-class="nameClass"></dataholder-details>
+				<dataholder-details :dataholder="dataholder" smallLogo :name-class="nameClass" class="font-body"></dataholder-details>
 				<div class="flex-none pl-2">
 					<i v-if="editing" @click="$emit('askDeselect', dataholder)" class="icon-rab-close icon-24 cursor-pointer"></i>
 					<i v-else class="icon-24" :class="[ disabled ? 'icon-rab-close-gray' : 'icon-rab-check' ]"></i>
