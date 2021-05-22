7cda678c9cc0dd09f20c53605b56b91050fa677f
camslice <cam@madebycam.com>
2021-05-22 17:36:37 +0700

    CardDataholderConnect v0.1.2

--- src/stories/molecules/CardDataholderConnect.js
+++ src/stories/molecules/CardDataholderConnect.js
@@ -4,12 +4,12 @@ ComponentVue_CardDataholderConnect
 		vueJSWidget.registerComponent('card-dataholder-connect', {
 */
 export default {
-	// v0.1.1
+	// v0.1.2
 	template: `
 		<div class="d-flex align-items-stretch">
 			<div class="rounded-lg border-light bg-brand-secondary-4 p-3 w-100">
 				<i v-if="dataholder.CDREnabled" class="icon-rab-logo-cdr-monogram icon-24 float-right"></i>
-				<dataholder-details :dataholder="dataholder" name-class="font-brand-bold h5 text-brand-1" class="mb-4"></dataholder-details>
+				<dataholder-details :dataholder="dataholder" name-class="font-brand-bold h5 text-brand-copy-1" class="mb-4"></dataholder-details>
 				<div class="font-brand text-brand-copy-1">
 					<div class="mb-1">Connection status</div>
 					<div>
