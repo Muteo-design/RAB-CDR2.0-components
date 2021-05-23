057bacc7cad71061405886546148fdafa04c77bf
camslice <cam@madebycam.com>
2021-05-23 23:53:26 +0700

    CardCDR v0.2.0

--- src/stories/molecules/CardCDR.js
+++ src/stories/molecules/CardCDR.js
@@ -4,14 +4,16 @@ ComponentVue_CardCDR
 		vueJSWidget.registerComponent('card-cdr', {
 */
 export default {
-	// v0.1.0
+	// v0.2.0
 	template: `
 		<div class="component-card-cdr rounded border-light px-2 pb-2 p-sm-3 text-left">
 			<div class="d-flex align-items-center pb-1 pb-sm-0">
 				<i class="icon-rab-logo-cdr"></i>
-				<div class="mt-3 mt-sm-0 ml-2 ml-sm-4">
-					<h5 class="my-0 line-height-3 h6 text-sm-h5">Regional Australia Bank</h5>
-					<p class="my-0 line-height-3 h7 text-sm-h6"><small class="text-dark">Accredited Data Recipient:<br class="d-sm-none"/> {{ adrNumber }}</small></p>
+				<div class="pt-1 mt-n3 ml-2 ml-sm-4">
+					<h5 class="mb-0 mt-4 mt-sm-2 line-height-3 h7 text-sm-h6">Regional Australia Bank</h5>
+					<p class="my-0 line-height-3 h9 text-black">Accredited Data Recipient:
+						<br class="d-sm-none"/>{{ adrNumber }}
+					</p>
 				</div>
 			</div>
 		</div>
