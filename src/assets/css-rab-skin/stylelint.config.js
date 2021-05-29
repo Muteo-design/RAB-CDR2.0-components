477b5ac722515f338a783e8e81e1ef2897656db6
camslice <cam@madebycam.com>
2021-05-29 23:45:28 +0700

    replace prettier with stylelint using --fix option to rewrite file FTW!

---
+++ src/assets/css-rab-skin/stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'indentation': 'tab',
+    'comment-empty-line-before': 'never',
+    'comment-whitespace-inside': null,
+    'rule-empty-line-before': 'never',
+    'no-descending-specificity': null,
+  },
+};
